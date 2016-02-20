14042cc4c177f4884aa4df3c97fdd7b2d283fa51
Dan Taylor <daniel.taylor@starkwood.co.uk>
2016-02-20 11:21:28 +0000

    Add ConsoleApp Package

---
+++ ITHedgeHog.Notifier/Options.cs
@@ -0,0 +1,33 @@
+﻿using CommandLine;
+using CommandLine.Text;
+
+namespace ITHedgeHog.Notifier
+{
+    internal class Options
+    {
+        [Option('a', "appid", Required = true, HelpText = "Application Id")]
+        public string AppId { get; set; }
+        [Option('d', "Description", Required = true, HelpText = "")]
+        public string Description { get; set; }
+        [Option('r', "revision", Required = false, HelpText = "Software Revision")]
+        public string Revision { get; set; }
+        [Option('c', "ChangeLog", Required = false)]
+        public string ChangeLog { get; set; }
+        [Option('u', "user", Required = true)]
+        public string User { get; set; }
+        [Option('n', "AppName", Required = true)]
+        public string AppName { get; set; }
+        [Option('k', "apikey", Required = true)]
+        public string ApiKey { get; set; }
+
+        [ParserState]
+        public IParserState LastParserState { get; set; }
+
+        [HelpOption]
+        public string GetUsage()
+        {
+            return HelpText.AutoBuild(this,
+              (HelpText current) => HelpText.DefaultParsingErrorsHandler(this, current));
+        }
+    }
+}
---
+++ ITHedgeHog.Notifier/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ITHedgeHog.Notifier
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var options = new Options();
+            if (CommandLine.Parser.Default.ParseArguments(args, options))
+            {
+
+                var client = new HttpClient();
+                client.DefaultRequestHeaders.Add("x-api-key", options.ApiKey);
+                // Create the HttpContent for the form to be posted.
+                var requestContent = new FormUrlEncodedContent(new[]
+                {
+                    new KeyValuePair<string, string>("deployment[app_name]", options.AppName),
+                    ////new KeyValuePair<string, string>("deployment[application_id]", txtAppId.Text), 
+                    new KeyValuePair<string, string>("deployment[description]", options.Description),
+                    new KeyValuePair<string, string>("deployment[revision]", options.Revision),
+                    new KeyValuePair<string, string>("deployment[changelog]", options.ChangeLog),
+                    new KeyValuePair<string, string>("deployment[user]", options.User),
+                });
+
+                // Get the response.
+                HttpResponseMessage response = client.PostAsync(
+                    "https://rpm.newrelic.com/deployments.xml",
+                    requestContent).Result;
+            }
+        }
+    }
+}
